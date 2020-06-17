d3ce5cb1b938cd44614a6d99a913aae2e3f1a658
Edson Santos <dev.escode@gmail.com>
2020-06-17 17:13:14 -0300

    Programa console para treinar, composicao, DateTime, Enums, StringBuilder

---
+++ Ex_SystemOrder/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex_SystemOrder.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Ex_SystemOrder/Entities/Order.cs
@@ -0,0 +1,63 @@
+﻿using Ex_SystemOrder.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Ex_SystemOrder.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            OrderItems.Add(orderItem);
+        }
+
+        public void RemoveItem(OrderItem orderItem)
+        {
+            OrderItems.Remove(orderItem);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach(OrderItem obj in OrderItems)
+            {
+                sum += obj.SubTotal();
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Order moment: {Moment.ToString("dd/MM/yyyy HH:mm:ss")}");
+            sb.AppendLine($"Order status: {Status}");
+            sb.AppendLine($"Client: {Client}");
+            sb.AppendLine("Order items:");
+            foreach (OrderItem order in OrderItems)
+            {
+                sb.AppendLine(order.ToString());
+            }
+            sb.AppendLine($"Total: {Total().ToString("F2", CultureInfo.InvariantCulture)}");
+            return sb.ToString();
+        }
+    }
+}
